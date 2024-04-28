b3f31deabcf7349bfb2b611f25aebd1c115d7794
chimexXy <moadzahnoun37@gmail.com>
2024-04-28 11:32:22 -0400

    web push

--- myWebsite/website.py
+++ myWebsite/website.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/T-shirt')
+def Tshirt():
+    return render_template('T-shirt.html')
+
 if __name__== '__main__':
     app.run(debug=True)
