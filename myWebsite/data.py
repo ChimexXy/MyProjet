e3ebbbe42dcb25e3fd5dfca043f738fd502e86d7
chimexXy <moadzahnoun37@gmail.com>
2024-04-29 07:18:43 -0400

    web

---
+++ myWebsite/data.py
@@ -0,0 +1,16 @@
+def Posts():
+    posts = [
+        {
+            'id': 1,
+            'title':'Anime'
+        },
+        {
+            'id': 1,
+            'title':'Sport'
+        },
+        {
+            'id': 1,
+            'title':'Autre'
+        },
+    ]
+    return posts
--- myWebsite/website.py
+++ myWebsite/website.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from data import Posts
 
 app = Flask(__name__)
+Posts = Posts()
 
 @app.route('/')
 def home():
@@ -9,6 +11,18 @@ def home():
 @app.route('/T-shirt')
 def Tshirt():
     return render_template('T-shirt.html')
+
+@app.route('/Sweats')
+def Sweats():
+    return render_template('Sweats.html')
+
+@app.route('/Your DTF')
+def yourdtf():
+    return render_template('dtf.html', posts = Posts)
+
+@app.route('/page')
+def page():
+    return render_template('page.html')
 
 if __name__== '__main__':
     app.run(debug=True)
